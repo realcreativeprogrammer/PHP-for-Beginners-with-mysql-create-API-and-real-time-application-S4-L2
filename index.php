aef3aa86059a0a2ed82dea5e863d4e04e69c323d
realcreativeprogrammer <realcreativeprogrammer@gmail.com>
2022-06-14 09:29:38 +0300

    FIRST

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo $_SERVER['SERVER_NAME'].'<br>';
+echo $_SERVER['HTTP_HOST'].'<br>';
+echo $_SERVER['PHP_SELF'].'<br>';
+echo $_SERVER['HTTP_USER_AGENT'].'<br>';
+echo $_SERVER['SCRIPT_NAME'].'<br>';
+echo $_SERVER['REQUEST_METHOD'].'<br>';
+echo $_SERVER['SERVER_PORT'].'<br>';
+
+?>
